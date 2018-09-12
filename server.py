aa1c03d95499b5b61b27cd327aa4d1ba397f0d7f
tatsunode <tatsunode@yahoo.co.jp>
2018-09-12 10:36:53 +0900

    fix tweet style

--- server.py
+++ server.py
@@ -5,6 +5,33 @@ from flask import Flask
 from flask import session, abort
 
 app = Flask(__name__)
+__style = """
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+	li {
+		list-style-type: none;
+	}
+    .AdaptiveMedia-photoContainer  {
+        padding: 8px;
+    }
+	.tweet {
+		background: #eeeeee;
+		padding: 16px;
+		margin: 16px;
+		border-radius: 5px;
+		box-shadow: 0 2px 5px #ccc;
+	}
+	.tweet-text {
+		background: #efefef;
+		padding: 4px;
+        font-size: 1.2rem;
+	}
+</style>
+"""
+
 
 @app.route("/tweet/<int:tweet_id>", methods=["GET"])
 def tweet(tweet_id):
@@ -24,21 +51,62 @@ def get_tweet_html(tweet_id):
 
     soup = BeautifulSoup(response.text, "html.parser")
 
+    metas = soup.find_all("meta")
+    for meta in metas:
+        meta.extract()
+
+    scripts = soup.find_all("script")
+    for script in scripts:
+        script.extract()
+
+    imgs = soup.find_all("img")
+    for img in imgs:
+        img["style"] = ""
+
     buttons = soup.find_all("button")
     for button in buttons:
         button.extract()
-
-    links = soup.find_all("a")
+    
+    links = soup.find_all("link")
     for link in links:
-        link.unwrap()
+        link.extract()
+
+    alinks = soup.find_all("a")
+    for alink in alinks:
+        alink.unwrap()
+
+    dismiss_modules = soup.find_all(attrs={"class": "dismiss-module"})
+    for dismiss_module in dismiss_modules:
+        dismiss_module.extract()
 
     footer = soup.find(attrs={"class":"stream-footer"})
     if footer is not None:
         footer.extract()
 
+    action = soup.find(attrs={"ProfileTweet-action"})
+    if action is not None:
+        action.extract()
+
+    sfc = soup.find(attrs={"class": "stream-fail-container"})
+    if sfc is not None:
+        sfc.extract()
+
+    adms = soup.find(attrs={"class": "AdaptiveMedia-singlePhoto"})
+    if adms is not None:
+        adms.unwrap()
+
+    follow_bar = soup.find(attrs={"class": "follow-bar"})
+    if follow_bar is not None:
+        follow_bar.extract()
+
     new_html = "<html lang='ja'>"
-    if soup.head is not None:
-        new_html += soup.head.prettify()
+    title = soup.find("title")
+    new_html += "<head>"
+    if title is not None:
+        new_html += title.prettify()
+    new_html += __style
+    new_html += "</head>"
+
     new_html += "<body>"
 
     main = soup.find(attrs={"role":"main"})
