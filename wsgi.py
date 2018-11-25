641e5cb833f390c297b26d0fea42c63e08065dc1
tatsunode <tatsunode@yahoo.co.jp>
2018-11-26 01:29:51 +0900

    wsgiを更新: apache側でsite-packageを指定するのではなくwsgi.pyで

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
 import sys
+import site
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+site.addsitedir(os.path.join(PROJECT_ROOT, "ENV", "lib", "python3.5", "site-packages"))
+
+sys.path.insert(0, PROJECT_ROOT)
 from server import app as application
