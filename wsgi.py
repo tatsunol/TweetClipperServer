dcdd4b6c265a9b5650104dd1027ace3484d56ecf
tatsunode <tatsunode@yahoo.co.jp>
2019-04-17 21:20:53 +0900

    update for docker

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # site.addsitedir(os.path.join(PROJECT_ROOT, "ENV", "lib", "python3.5", "site-packages"))
 
-# sys.path.insert(0, PROJECT_ROOT)
+sys.path.insert(0, PROJECT_ROOT)
 from server import app as application
